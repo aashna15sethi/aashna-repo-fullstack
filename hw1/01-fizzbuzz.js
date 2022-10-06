af92c126d82a47a130f218edb0754c19cc5c2e2c
Aashna <aashna@pdx.edu>
2022-10-05 17:26:08 -0700

    added functionality to fizzbuzz

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -11,7 +11,24 @@ Use console.log() to write the proper output to the command line.
 
 const fizzbuzz = () => {
   // Add your code here
-};
+  for (i = 1; i <= 100; i++)
+  {
+    if(i % 5 == 0 && i % 3 ==0){
+      console.log("fizzbuzz")
+    }
+    else if(i % 3 != 0 && i % 5 ==0){
+      console.log("buzz")
+    }
+    else if (i % 3 == 0)
+    {
+      console.log("fizz")
+    }  
+    else{
+      console.log(i)
+    }
+    
+  }
+  }
 
 fizzbuzz();
 // 1
