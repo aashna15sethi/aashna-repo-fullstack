8c05e5eb1762908d496b44264dab804952f568e5
Aashna <aashna@pdx.edu>
2022-11-03 13:17:22 -0700

    changes made to hw3

--- hw3/02-url.js
+++ hw3/02-url.js
@@ -1,5 +1,7 @@
 const http = require('http');
-const port = process.env.PORT || 5001;
+const url = require('url');
+
+const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   const routes = [
@@ -7,7 +9,6 @@ const server = http.createServer((req, res) => {
     '/items?first=1&second=2&third=3&fourth=4',
     '/characters?spongebob=squarepants&patrick=star&sandy=cheeks',
   ];
-
   // use the URL interface to work with URLs
   // source: https://developer.mozilla.org/en-US/docs/Web/API/URL
   let url = new URL(req.url, `http://${req.headers.host}`);
