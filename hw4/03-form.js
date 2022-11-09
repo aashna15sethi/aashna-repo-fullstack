6da7be7f12d1fc5631aa786253e76f5046c72a39
Aashna <aashna@pdx.edu>
2022-11-09 03:02:12 -0800

    final changes to form.js

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const static = require("node-static");
+const querystring = require("node:querystring");
 const port = process.env.PORT || 5000;
 
 // Use middleware static() to serve all static files in the given folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Use middleware urlencoded() to parse an incoming request with a urlencoded payload and return an objectß
 app.use(express.urlencoded({ extended: false }));
 
 // POST request
-app.post('', (req, res) => {
+app.post("", (req, res) => {
   // Add your code here
 });
 
