f0b3afa2674babc2bcb946f3b0d472dc1679b795
Aashna <aashna@pdx.edu>
2022-11-09 21:57:18 -0800

    format changes to form.js

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const static = require("node-static");
+// const static = require("node-static");
 const querystring = require("node:querystring");
 const port = process.env.PORT || 5000;
 
