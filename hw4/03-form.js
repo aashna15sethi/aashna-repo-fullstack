8bcfe58704410bd7ca74a05b4e00b8af6a13d690
Aashna <aashna@pdx.edu>
2022-11-09 23:26:04 -0800

    made changes to form.js

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-// const static = require("node-static");
-const querystring = require("node:querystring");
+const htmlFilePath = require("path");
 const port = process.env.PORT || 5000;
 
 // Use middleware static() to serve all static files in the given folder
@@ -11,8 +10,37 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 // POST request
-app.post("", (req, res) => {
+app.get("/", (req, res) => {
+  res.redirect("/form");
+});
+
+app.get("/form", (req, res) => {
+  res.sendFile(htmlFilePath.join(__dirname + "/public/index.html"));
+});
+
+app.post("/submit", (req, res) => {
   // Add your code here
+  const { fname, email, comments, yesCheck } = req.body;
+
+  if (comments == "") {
+    req.body.comments = "n/a";
+  } else {
+    req.body.comments = comments;
+  }
+  if (yesCheck == "yes") {
+    req.body.yesCheck = "Yes, sign me up for the newsletter";
+  } else {
+    req.body.yesCheck = "No, thank you";
+  }
+
+  console.log(req.body);
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`<h2> Your details </h2>
+                   <p> Name: ${fname} </p>
+                   <p> Email: ${email} </p>
+                   <p> Comments: ${req.body.comments}  </p>
+                   <p> Newsletter: ${req.body.yesCheck} </p>`);
+  res.end();
 });
 
 app.listen(port, () => {
