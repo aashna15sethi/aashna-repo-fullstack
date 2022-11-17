f85bcf04d91e7dea359793891284e8d1649b64a2
Aashna <aashna@pdx.edu>
2022-11-16 20:31:16 -0500

    changes

--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -1,15 +1,43 @@
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Add your code here
-
 // Use the express-session module
-app.use(/** ... */);
+app.use(
+  session({
+    store: new session.MemoryStore(),
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 86400000,
+    },
+  })
+);
 
-app.get('/', (req, res) => {
-  // Add your code here
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`<p>Currently on route : ${req.url}</p></br>`);
+  req.session.lastVisit = [];
+  if (req.url !== "/favicon.ico") {
+    req.session.lastVisit.push(req.url);
+    console.log("req.session.lastVisit ", req.session.lastVisit);
+    if (req.session.lastVisit) {
+      console.log("Currently on route : ", req.url);
+      res.write(`Previouls Visited`);
+      req.session.lastVisit.push(`${req.url}`);
+      req.session.lastVisit.forEach((route) => {
+        res.write(`<br>${route}`);
+        console.log("req.session.lastVisit", route);
+      });
+    } else {
+      req.session.lastVisit = [];
+      req.session.lastVisit.push(`${req.url}`);
+      res.write(`Welcome to http://${req.headers.host}`);
+    }
+  }
+  res.end();
 });
 
 app.listen(port, () => {
